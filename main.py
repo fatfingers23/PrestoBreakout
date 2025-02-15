6582695ab5bcc5723ace75720afa4a5061d2dd2d
Bailey Townsend <baileytownsend2323@gmail.com>
2025-02-15 17:17:27 -0600

    lol decent

--- main.py
+++ main.py
@@ -11,21 +11,17 @@ from utime import ticks_ms, ticks_diff
 class Ball(object):
     """Ball."""
 
-    def __init__(self, x, y, x_speed, y_speed, display, radius=4, frozen=False):
-        """Initialize ball.
-
-        Args:
-            x, y (int): Initial position of the ball (center of the circle).
-            x_speed, y_speed (int): Initial speed of the ball.
-            display (PRESTO display): Presto display object.
-            radius (int): Radius of the ball.
-            frozen (bool): Whether the ball is frozen to the paddle.
-        """
+    def __init__(self, x, y, x_speed, y_speed, display, width=10, height=10,
+                 frozen=False):
         self.x = x
         self.y = y
-        self.radius = radius
+        self.x2 = x + width - 1
+        self.y2 = y + height - 1
         self.prev_x = x
         self.prev_y = y
+        self.width = width
+        self.height = height
+        self.center = width // 2
         self.max_x_speed = 3
         self.max_y_speed = 3
         self.frozen = frozen
@@ -35,54 +31,40 @@ class Ball(object):
         self.x_speed2 = 0.0
         self.y_speed2 = 0.0
         self.created = ticks_ms()
-        self.ball_color = display.create_pen(255, 0, 0)  # Red color for the ball
-        self.background_color = display.create_pen(255, 255, 255)  # White color for the background
-        self.display_width = 240
-        self.display_height = 240
+        self.color = display.create_pen(0, 0, 0)
+        self.background_color = display.create_pen(255, 255, 255)
 
     def clear(self):
         """Clear ball."""
         self.display.set_pen(self.background_color)
-        self.display.circle(self.x, self.y, self.radius)
+        display.rectangle(self.x, self.y, self.width, self.height)
 
     def clear_previous(self):
-        """Clear previous ball position."""
+        """Clear prevous ball position."""
         self.display.set_pen(self.background_color)
-        self.display.circle(self.prev_x, self.prev_y, self.radius)
+        display.rectangle(self.prev_x, self.prev_y, self.width, self.height)
 
     def draw(self):
         """Draw ball."""
         self.clear_previous()
-        self.display.set_pen(self.ball_color)
-        self.display.circle(self.x, self.y, self.radius)
+        self.display.set_pen(self.color)
+        display.rectangle(self.x, self.y, self.width, self.height)
 
     def set_position(self, paddle_x, paddle_y, paddle_x2, paddle_center):
-        """Set ball position and handle collisions.
-
-        Args:
-            paddle_x, paddle_y (int): Paddle position.
-            paddle_x2 (int): Paddle right edge.
-            paddle_center (int): Paddle center.
-
-        Returns:
-            bool: True if the ball bounced, False otherwise.
-        """
         bounced = False
+        """Set ball position."""
         self.prev_x = self.x
         self.prev_y = self.y
-
         # Check if frozen to paddle
         if self.frozen:
             # Freeze ball to top center of paddle
-            self.x = paddle_x + (paddle_center - self.radius)
-            self.y = paddle_y - self.radius
+            self.x = paddle_x + (paddle_center - self.center)
+            self.y = paddle_y - self.height
             if ticks_diff(ticks_ms(), self.created) >= 2000:
                 # Release frozen ball after 2 seconds
                 self.frozen = False
             else:
-                return bounced
-
-        # Update ball position
+                return
         self.x += int(self.x_speed) + int(self.x_speed2)
         self.x_speed2 -= int(self.x_speed2)
         self.x_speed2 += self.x_speed - int(self.x_speed)
@@ -91,33 +73,36 @@ class Ball(object):
         self.y_speed2 -= int(self.y_speed2)
         self.y_speed2 += self.y_speed - int(self.y_speed)
 
-        # Bounce off walls
-        if self.y - self.radius < 10:  # Top wall
-            self.y = 10 + self.radius
+        far_right_wall = 240
+        # Bounces off walls
+        if self.y < 30:
+            self.y = 10
             self.y_speed = -self.y_speed
             bounced = True
-        if self.x + self.radius >= self.display_width - 10:  # Right wall
-            self.x = self.display_width - 10 - self.radius
+        if self.x + self.width >= far_right_wall:
+            self.x = far_right_wall - self.width
             self.x_speed = -self.x_speed
             bounced = True
-        elif self.x - self.radius < 10:  # Left wall
-            self.x = 10 + self.radius
+        elif self.x < 3:
+            self.x = 3
             self.x_speed = -self.x_speed
             bounced = True
 
-        # Check for collision with paddle
-        if (self.y + self.radius >= paddle_y and
-                self.x - self.radius <= paddle_x2 and
-                self.x + self.radius >= paddle_x):
+        # Check for collision with Paddle
+        if (self.y2 >= paddle_y and
+           self.x <= paddle_x2 and
+           self.x2 >= paddle_x):
             # Ball bounces off paddle
-            self.y = paddle_y - (self.radius + 1)
-            ratio = ((self.x) - (paddle_x + paddle_center)) / paddle_center
+            self.y = paddle_y - (self.height + 1)
+            ratio = ((self.x + self.center) -
+                     (paddle_x + paddle_center)) / paddle_center
             self.x_speed = ratio * self.max_x_speed
             self.y_speed = -sqrt(max(1, self.max_y_speed ** 2 - self.x_speed ** 2))
             bounced = True
 
+        self.x2 = self.x + self.width - 1
+        self.y2 = self.y + self.height - 1
         return bounced
-
 
 class Paddle(object):
     """Paddle."""
@@ -297,6 +282,47 @@ class Life(object):
         display.rectangle(self.x, self.y, self.width, self.height)
         # self.display.rect(self.x, self.y, self.width, self.height, 1)
 
+class Score(object):
+    """Score."""
+
+    def __init__(self, display):
+        """Initialize score.
+
+        Args:
+            display (SSD1306): OLED g.display.
+        """
+        margin = 40
+        self.display = display
+        display.text("Score:", 180, 8, scale=0)
+        # self.display.text('S:', margin, 0, 1)
+        self.x = 180 + margin
+        self.y = 8
+        self.value = 0
+        self.background_color = display.create_pen(255, 255, 255)
+        self.color = display.create_pen(0, 0, 0)
+        self.draw()
+
+    def draw(self):
+        """Draw score value."""
+        self.display.set_pen(self.background_color)
+        display.rectangle(self.x, self.y, 20, 10)
+        self.display.set_pen(self.color)
+        display.text(str(self.value), self.x, self.y, scale=0)
+
+        # self.display.text( str(self.value), self.x, self.y,1)
+
+    def game_over(self):
+        """Display game_over."""
+        self.display.set_pen(self.color)
+        self.display.text("GAME OVER", 90, 120, scale=2)
+        self.display.text("Press C to start", 0, 20)
+        # self.display.text('GAME OVER', (self.display.width // 2) - 30,
+        #                        int(self.display.height / 1.5), 1)
+
+    def increment(self, points):
+        """Increase score by specified points."""
+        self.value += points
+        self.draw()
 
 def load_level(level, display, level_color):
     global frameRate
@@ -365,7 +391,7 @@ while not exitGame:
         display.clear()
         display.set_pen(BLACK)
         display.text("BREAKOUT", 0, 0, scale=2)
-        display.text("Press C to start", 100, 0)
+        display.text("Press C to start", 0, 20)
         if nc.buttons.C:
             break
         presto.update()
@@ -379,10 +405,13 @@ while not exitGame:
 
         # Initialize paddle
         paddle = Paddle(display, paddle_width, 10)
+        # Initialize score
+        score = Score(display)
+
         # Initialize balls
         balls = []
         # Add first ball
-        balls.append(Ball(59, 58, -2, -1, display, frozen=True))
+        balls.append(Ball(120, 120, -2, -1, display, frozen=True))
         # Initialize lives
         lives = []
         for i in range(0, 3):
@@ -411,49 +440,94 @@ while not exitGame:
                 paddle_vect *= 5
             paddle.h_position(paddle.x + paddle_vect)
             prev_paddle_vect = paddle_vect
-            presto.update()
 
-        # if usePaddle :
-# while True:
-#     # touch.poll()
-#     x, y = nc.joystick
-#     move_amount = 0
-#     # if x > 127:
-#     #     prev_paddle_vect = 1
-#     # elif x < 127:
-#     #     prev_paddle_vect = -1
-#
-#     paddle_vect = 0
-#     if x < 127:
-#         paddle_vect = -1
-#     elif x > 127:
-#         paddle_vect = 1
-#     if paddle_vect != prev_paddle_vect:
-#         paddle_vect *= 3
-#     else:
-#         paddle_vect *= 5
-#     PADDLE.h_position(PADDLE.x + paddle_vect)
-#     prev_paddle_vect = paddle_vect
-#
-#
-#     # ax, ay, az = nc.acceleration
-#     # print("joystick = {},{}".format(x, y))
-#     # print("accceleration ax={}, ay={}, az={}".format(ax, ay, az))
-#
-#     count += 1
-#     display.set_pen(WHITE)
-#     display.set_layer(0)
-#     display.clear()
-#     for brick in bricks:
-#         brick.draw()
-#
-#     PADDLE.draw()
-#
-#     # Finally we update the screen with our changes :)
-#     presto.update()
-#
-#
 
+
+            # Handle balls
+            score_points = 0
+            for ball in balls:
+                #True it hits something
+                ball.set_position(paddle.x, paddle.y,paddle.x2, paddle.center)
+                # move ball and check if bounced off walls and paddle
+                # if ball.set_position(paddle.x, paddle.y, paddle.x2, paddle.center):
+                    # g.playSound(900, 10)
+                # Check for collision with bricks if not frozen
+                if not ball.frozen:
+                    prior_collision = False
+                    ball_x = ball.x
+                    ball_y = ball.y
+                    ball_x2 = ball.x2
+                    ball_y2 = ball.y2
+                    ball_center_x = ball.x + ((ball.x2 + 1 - ball.x) // 2)
+                    ball_center_y = ball.y + ((ball.y2 + 1 - ball.y) // 2)
+
+                    # Check for hits
+                    for brick in bricks:
+                        if (ball_x2 >= brick.x and
+                                ball_x <= brick.x2 and
+                                ball_y2 >= brick.y and
+                                ball_y <= brick.y2):
+                            # Hit
+                            if not prior_collision:
+                                ball.x_speed, ball.y_speed = brick.bounce(
+                                    ball.x,
+                                    ball.y,
+                                    ball.x2,
+                                    ball.y2,
+                                    ball.x_speed,
+                                    ball.y_speed,
+                                    ball_center_x,
+                                    ball_center_y)
+                                # g.playTone('c6', 10)
+                                prior_collision = True
+                            score_points += 1
+                            brick.clear()
+                            bricks.remove(brick)
+
+                    # Check for missed
+                if ball.y2 > HEIGHT - 2:
+                    ball.clear_previous()
+                    balls.remove(ball)
+                    if not balls:
+                        # Lose life if last ball on screen
+                        if len(lives) == 0:
+                            score.game_over()
+                            while True:
+                                if nc.buttons.C:
+                                    break
+
+                            # g.playTone('g4', 500)
+                            # g.playTone('c5', 200)
+                            # g.playTone('f4', 500)
+                            gameOver = True
+                        else:
+                            # Subtract Life
+                            lives.pop().clear()
+                            # Add ball
+                            balls.append(Ball(59, 58, 2, -3, display,
+                                              frozen=True))
+                else:
+                    # Draw ball
+                    ball.draw()
+                    # Update score if changed
+                if score_points:
+                    score.increment(score_points)
+                # Check for level completion
+                if not bricks:
+                    for ball in balls:
+                        ball.clear()
+                    balls.clear()
+                    level += 1
+                    paddle_width -= 2
+                    if level > MAX_LEVEL:
+                        level = 1
+                    bricks = load_level(level, g.display)
+                    balls.append(Ball(59, 58, -2, -1, display, frozen=True))
+
+                presto.update()
+
+
+        # for ball in balls:
 
 
 
